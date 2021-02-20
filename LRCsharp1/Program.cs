4b43b1eb03e016d74eb8a30e817ec6314abe5914
Nikita Pestrak <pestraknv@yandex.ru>
2021-02-20 23:50:49 +0300

    add deck and start words

--- LRCsharp1/Program.cs
+++ LRCsharp1/Program.cs
@@ -6,6 +6,21 @@ namespace LRCsharp1
     {
         static void Main(string[] args)
         {
+            int[] deck = new int[]
+            {
+                6, 6, 6, 6, // 6
+                7, 7, 7, 7, // 7
+                8, 8, 8, 8, // 8
+                9, 9, 9, 9, // 9
+                10, 10, 10, 10, // 10
+                11, 11, 11, 11, // Jack
+                12, 12, 12, 12, // Lady
+                13, 13, 13, 13, // King
+                14, 14, 14, 14 // Ace
+            };
+            
+            Console.WriteLine("Hello, this is a popular children's game drunkard");
+            Console.WriteLine("This game is played by two bots, you can watch them play");
             
         }
     }
