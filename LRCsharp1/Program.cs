346ce3808eb9579ac7071b5ef5e22dbc7220e1c7
Nikita Pestrak <pestraknv@yandex.ru>
2021-02-21 00:06:28 +0300

    Revert "add deck and start words"
    This reverts commit 22086c5276d9f2a3c05ffe9dd83d1da68559089e.

--- LRCsharp1/Program.cs
+++ LRCsharp1/Program.cs
@@ -4,24 +4,6 @@ namespace LRCsharp1
 {
     class Program
     {
-        static void CheckCardsRepetition(int[] takeDeck)
-        {
-            Random obj = new Random();
-
-            for (int i = 0; i < 36; i++)
-            {
-                for (int j = 0; j < i; j++)
-                {
-                    if (takeDeck[i] == takeDeck[j])
-                    {
-                        takeDeck[i] = obj.Next(36);
-
-                        CheckCardsRepetition(takeDeck);
-                    }
-                }
-            }
-        }
-        
         static void Main(string[] args)
         {
             int[] deck = new int[]
@@ -37,23 +19,8 @@ namespace LRCsharp1
                 14, 14, 14, 14 // Ace
             };
             
-            int[] takeDeck = new int[36];
-
-            Random obj = new Random();
-            
-            for (int i = 0; i < 36; i++)
-            {
-                takeDeck[i] = obj.Next(36);
-            }
-            
-            CheckCardsRepetition(takeDeck);
-            
             Console.WriteLine("Hello, this is a popular children's game drunkard");
             Console.WriteLine("This game is played by two bots, you can watch them play");
-            Console.WriteLine("Do you want to see cards and operations? (1 - yes / 0 - no)");
-            string decision = Console.ReadLine();
-            int decisionInt = Convert.ToInt32(decision);
-            
             
         }
     }
